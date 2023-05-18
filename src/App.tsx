1cf2792dec16f063762ce6c9d64c35d12eed1110
Antonio Forte <anto4te@Antonios-MacBook-Pro.fritz.box>
2023-05-18 08:34:00 +0200

    Getting Started

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen flex items-center justify-center">
+        <h1>Lampoo</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
